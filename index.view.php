6b9ed18f7ced872171aa64f0ec946309b6dabca1
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-03 18:41:58 +0800

    feat: add classes and script imports

--- index.view.php
+++ index.view.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
+<!DOCTYPE html class>
+<html lang="en" class="h-full bg-gray-100">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laracasts - PHP for Beginners</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body>
-    <!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
-    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
-    <!--
-    This example requires updating your template:
-
-    ```
-    <html class="h-full bg-gray-100">
-    <body class="h-full">
-    ```
-    -->
+<body class="h-full">
     <div class="min-h-full">
         <nav class="bg-gray-800">
             <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
