d06e94fc1dec64608eeaf28092001c348ea3d1aa
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-04 17:45:57 +0800

    feat: add head and footer partials, apply to views

--- views/index.view.php
+++ views/index.view.php
@@ -1,30 +1,15 @@
-<!DOCTYPE html class>
-<html lang="en" class="h-full bg-gray-100">
+<?php require("partials/head.php") ?>
+<?php require("partials/nav.php") ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laracasts - PHP for Beginners</title>
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
-    <script src="https://cdn.tailwindcss.com"></script>
-</head>
-
-<body class="h-full">
-    <div class="min-h-full">
-        <?php require("partials/nav.php") ?>
-
-        <header class="relative bg-white shadow-sm">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
-            </div>
-        </header>
-        <main>
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <!-- Your content -->
-                You are now in the Home page.
-            </div>
-        </main>
+<header class="relative bg-white shadow-sm">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
     </div>
-</body>
-
-</html>
+</header>
+<main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <!-- Your content -->
+        You are now in the Home page.
+    </div>
+</main>
+<?php require("partials/footer.php") ?>
---
+++ views/partials/head.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html class>
+<html lang="en" class="h-full bg-gray-100">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laracasts - PHP for Beginners</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="h-full">
+    <div class="min-h-full">
