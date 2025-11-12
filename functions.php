1cefc0fd77aa14147434491b48b70a40b5d74403
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-12 19:15:46 +0800

    feat: create functions php

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function dd($value) {
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+    die();
+}
+
+// Navbar partial
+$current = "bg-gray-900 text-white";
+$default = "text-gray-300 hover:bg-white/5 hover:text-white";
+
+function isCurrentPage($url) {
+    return $_SERVER["REQUEST_URI"] === $url;
+}
