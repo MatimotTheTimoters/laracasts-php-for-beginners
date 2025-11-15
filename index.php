52dceca68f4de34c495549b1584c0f953c443c22
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-15 13:56:14 +0800

    feat: create and implement abort function

--- index.php
+++ index.php
@@ -13,7 +13,13 @@ $routes = [
 if (array_key_exists($uri, $routes)) {
     require $routes[$uri];
 } else {
-    http_response_code(404);
-    require 'views/404.php';
-    exit;
+    abort();
+}
+
+function abort($code = 404) {
+    http_response_code($code);
+
+    require "views/{$code}.php";
+
+    die();
 }
