b97c95f2dcc0a0706c8b952246957a5ee44cc4c4
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-15 14:00:26 +0800

    refactor: create router and move code

--- index.php
+++ index.php
@@ -2,24 +2,4 @@
 
 require 'functions.php';
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-];
-
-if (array_key_exists($uri, $routes)) {
-    require $routes[$uri];
-} else {
-    abort();
-}
-
-function abort($code = 404) {
-    http_response_code($code);
-
-    require "views/{$code}.php";
-
-    die();
-}
+require 'router.php';
