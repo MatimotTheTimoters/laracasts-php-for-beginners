a0252c96f85db76ea103bbf16604e0115cbd00be
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-17 07:28:11 +0800

    refactor: index to just use db file

--- index.php
+++ index.php
@@ -1,19 +1,9 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
 
-// require 'router.php';
+$db = new Database();
+$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
 
-$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4';
-$username = 'root';
-
-$pdo = new PDO($dsn, $username);
-
-$statement = $pdo->prepare("select * from posts");
-$statement->execute();
-
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($posts as $post) {
-    echo "<li>{$post['title']}</li>";
-}
+dd($posts);
