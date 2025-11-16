8f29099606974ba4d7d4c832f56dfadd9884d052
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-16 14:47:12 +0800

    test: connection to myapp db via select query

--- index.php
+++ index.php
@@ -2,4 +2,18 @@
 
 require 'functions.php';
 
-require 'router.php';
+// require 'router.php';
+
+$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4';
+$username = 'root';
+
+$pdo = new PDO($dsn, $username);
+
+$statement = $pdo->prepare("select * from posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+foreach ($posts as $post) {
+    echo "<li>{$post['title']}</li>";
+}
