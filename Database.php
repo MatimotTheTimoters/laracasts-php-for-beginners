bf3f1ed29f7a498ff31146b81c70b6da79ac1203
Chonky_Seal <MatimotTheTimoters@gmail.com>
2025-11-17 07:27:56 +0800

    feat: create Database class file

---
+++ Database.php
@@ -0,0 +1,20 @@
+<?php
+
+class Database {
+    public $connection;
+
+    public function __construct() 
+    {
+        $dsn = 'mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4';
+
+        $this->connection = new PDO($dsn);
+    }
+
+    public function query($sqlQuery) 
+    {
+        $statement = $this->connection->prepare($sqlQuery);
+        $statement->execute();
+
+        return $statement;
+    }
+}
